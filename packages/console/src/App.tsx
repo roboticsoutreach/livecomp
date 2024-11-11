790e8b0270e5b262aa0141fedb8907dad67133c5
Alex Colville <alexcolville.mrpc@gmail.com>
2024-11-11 22:35:47 +0000

    chore: switch to more verbose page naming

--- packages/console/src/App.tsx
+++ packages/console/src/App.tsx
@@ -1,11 +1,11 @@
 import { createBrowserRouter, Navigate, RouterProvider } from "react-router-dom";
-import LoginPage from "./pages/auth/login";
+import LoginPage from "./pages/auth/LoginPage";
 import ProtectedRoute from "./components/util/ProtectedRoute";
-import DashboardPage from "./pages/dashboard";
-import CompetitionsPage from "./pages/competitions";
-import CreateCompetitionPage from "./pages/competitions/create";
-import GamesPage from "./pages/games";
-import ViewGamePage from "./pages/games/view";
+import DashboardPage from "./pages/DashboardPage";
+import CompetitionsPage from "./pages/competitions/CompetitionsPage";
+import CreateCompetitionPage from "./pages/competitions/CreateCompetitionPage";
+import GamesPage from "./pages/games/GamesPage";
+import ViewGamePage from "./pages/games/ViewGamePage";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { api, queryClient } from "./utils/trpc";
 import { AuthContext } from "./utils/context";
