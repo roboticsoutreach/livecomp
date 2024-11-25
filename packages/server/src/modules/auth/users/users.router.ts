b8454498054fe00143a5c58ba1aff1a30bc7fe48
Alex Colville <alexcolville.mrpc@gmail.com>
2024-11-25 16:41:13 +0000

    feat: add user management routes

--- packages/server/src/modules/auth/users/users.router.ts
+++ packages/server/src/modules/auth/users/users.router.ts
@@ -1,6 +1,55 @@
-import { protectedProcedure, router } from "../../../trpc/trpc";
+import { z } from "zod";
+import { protectedProcedure, restrictedProcedure, router } from "../../../trpc/trpc";
+import { insertUserSchema, userPasswords, users } from "../../../db/schema/auth";
+import { usersRepository } from "./users.repository";
+import { TRPCError } from "@trpc/server";
+import { eq } from "drizzle-orm";
+
+function generatePassword() {
+    return Math.random().toString(36).slice(-10);
+}
 
 export const usersRouter = router({
+    create: restrictedProcedure("sysadmin")
+        .input(z.object({ data: insertUserSchema }))
+        .mutation(async ({ ctx, input }) => {
+            const user = await usersRepository.create(input.data);
+
+            if (!user) {
+                throw new TRPCError({ code: "INTERNAL_SERVER_ERROR", message: "Failed to create user" });
+            }
+
+            const password = generatePassword();
+
+            await ctx.db.insert(userPasswords).values({
+                passwordHash: await Bun.password.hash(password),
+                userId: user.id,
+            });
+
+            return {
+                user,
+                password,
+            };
+        }),
+
+    fetchAll: restrictedProcedure("sysadmin").query(async () => await usersRepository.findMany()),
+
+    fetchById: restrictedProcedure("sysadmin")
+        .input(z.object({ id: z.string() }))
+        .query(async ({ input }) => await usersRepository.findFirst({ where: eq(users.id, input.id) })),
+
     fetchCurrent: protectedProcedure.query(({ ctx }) => ctx.user),
+
+    update: restrictedProcedure("sysadmin")
+        .input(z.object({ id: z.string(), data: insertUserSchema.partial() }))
+        .mutation(async ({ input }) => {
+            return await usersRepository.update(input.data, { where: eq(users.id, input.id) });
+        }),
+
+    delete: restrictedProcedure("sysadmin")
+        .input(z.object({ id: z.string() }))
+        .mutation(async ({ input }) => {
+            return await usersRepository.delete({ where: eq(users.id, input.id) });
+        }),
 });
 
