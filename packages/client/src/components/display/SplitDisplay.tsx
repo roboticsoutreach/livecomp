a2064c55adad719e35f1fbba7bec8805f6c1a0a0
Alex Colville <56324439+Alexbruvv@users.noreply.github.com>
2024-12-10 12:13:56 +0000

    Change next matches display

--- packages/client/src/components/display/SplitDisplay.tsx
+++ packages/client/src/components/display/SplitDisplay.tsx
@@ -3,28 +3,30 @@ import { PropsWithChildren } from "react";
 export default function SplitDisplay({ children }: PropsWithChildren) {
     return (
         <div className="w-screen h-screen flex flex-row">
-            <div className="w-4/5 h-full p-4">{children}</div>
-            <div className="w-1/5 h-full border-l-2 flex flex-col">
+            <div className="w-2/3 h-full p-4">{children}</div>
+            <div className="w-1/3 h-full border-l-2 flex flex-col">
                 <div className="text-white text-4xl p-4 font-bold bg-slate-600">Next matches</div>
 
-                <table className="w-full">
+                <table className="w-full border-b-2 border-white">
                     <thead className="bg-slate-600">
                         <tr>
-                            <th className="w-1/4 p-4 text-left text-2xl text-white">Team</th>
-                            <th className="w-1/4 p-4 text-left text-2xl text-white">Time</th>
-                            <th className="w-1/4 p-4 text-left text-2xl text-white">Team</th>
-                            <th className="w-1/4 p-4 text-left text-2xl text-white">Time</th>
+                            <th className="w-1/6 p-4 text-left text-2xl text-white">Team</th>
+                            <th className="w-1/6 p-4 text-left text-2xl text-white">Time</th>
+                            <th className="w-1/6 p-4 text-left text-2xl text-white">Team</th>
+                            <th className="w-1/6 p-4 text-left text-2xl text-white">Time</th>
+                            <th className="w-1/6 p-4 text-left text-2xl text-white">Team</th>
+                            <th className="w-1/6 p-4 text-left text-2xl text-white">Time</th>
                         </tr>
                     </thead>
 
                     <tbody>
-                        {[...Array(15).keys()].map((i) => {
+                        {[...Array(10).keys()].map((i) => {
                             const shadeLeft = i % 2 === 0;
 
                             return (
                                 <tr key={i}>
                                     <td className={`p-4 text-lg text-white ${shadeLeft ? "bg-slate-900" : ""}`}>
-                                        T{(i * 2 + 1).toString().padStart(2, "0")}
+                                        T{(i * 3 + 1).toString().padStart(2, "0")}
                                     </td>
                                     <td
                                         className={`p-4 text-lg text-white font-mono ${shadeLeft ? "bg-slate-900" : ""}`}
@@ -32,10 +34,18 @@ export default function SplitDisplay({ children }: PropsWithChildren) {
                                         14:35
                                     </td>
                                     <td className={`p-4 text-lg text-white ${!shadeLeft ? "bg-slate-900" : ""}`}>
-                                        T{(i * 2 + 2).toString().padStart(2, "0")}
+                                        T{(i * 3 + 2).toString().padStart(2, "0")}
                                     </td>
                                     <td
                                         className={`p-4 text-lg text-white font-mono ${!shadeLeft ? "bg-slate-900" : ""}`}
+                                    >
+                                        14:35
+                                    </td>
+                                    <td className={`p-4 text-lg text-white ${shadeLeft ? "bg-slate-900" : ""}`}>
+                                        T{(i * 3 + 3).toString().padStart(2, "0")}
+                                    </td>
+                                    <td
+                                        className={`p-4 text-lg text-white font-mono ${shadeLeft ? "bg-slate-900" : ""}`}
                                     >
                                         14:35
                                     </td>
@@ -43,6 +53,76 @@ export default function SplitDisplay({ children }: PropsWithChildren) {
                             );
                         })}
                     </tbody>
+                </table>
+
+                <table className="w-full my-auto">
+                    <tr>
+                        <td className="w-1/3">
+                            <h1 className="text-white font-bold text-4xl p-4 text-center">
+                                Current
+                                <br />
+                                match
+                            </h1>
+                        </td>
+                        <td className="w-2/3">
+                            <div className="w-auto h-1/5 p-2 m-6 border-2 flex flex-col gap-2">
+                                <h1 className="text-white text-xl font-semibold">
+                                    <span className="float-start">Match 0</span>
+                                    <span className="float-end">13:45</span>
+                                </h1>
+                                <div className="flex-grow">
+                                    <div className="w-full h-full grid grid-cols-2">
+                                        <div className="bg-orange-600 content-center">
+                                            <h2 className="text-white font-bold text-3xl text-center">T01</h2>
+                                        </div>
+                                        <div className="bg-green-600 content-center">
+                                            <h2 className="text-white font-bold text-3xl text-center">T02</h2>
+                                        </div>
+                                        <div className="bg-pink-600 content-center">
+                                            <h2 className="text-white font-bold text-3xl text-center">T03</h2>
+                                        </div>
+                                        <div className="bg-yellow-600 content-center">
+                                            <h2 className="text-white font-bold text-3xl text-center">T04</h2>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td className="w-1/3">
+                            <h1 className="text-white font-bold text-4xl p-4 text-center">
+                                Next
+                                <br />
+                                match
+                            </h1>
+                        </td>
+                        <td className="w-2/3">
+                            <div className="w-auto h-1/5 p-2 m-6 border-2 flex flex-col gap-2">
+                                <h1 className="text-white text-xl font-semibold">
+                                    <span className="float-start">Match 1</span>
+                                    <span className="float-end">13:50</span>
+                                </h1>
+                                <div className="flex-grow">
+                                    <div className="w-full h-full grid grid-cols-2">
+                                        <div className="bg-orange-600 content-center">
+                                            <h2 className="text-white font-bold text-3xl text-center">T05</h2>
+                                        </div>
+                                        <div className="bg-green-600 content-center">
+                                            <h2 className="text-white font-bold text-3xl text-center">T06</h2>
+                                        </div>
+                                        <div className="bg-pink-600 content-center">
+                                            <h2 className="text-white font-bold text-3xl text-center">T07</h2>
+                                        </div>
+                                        <div className="bg-yellow-600 content-center">
+                                            <h2 className="text-white font-bold text-3xl text-center">T08</h2>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </td>
+                    </tr>
                 </table>
             </div>
         </div>
