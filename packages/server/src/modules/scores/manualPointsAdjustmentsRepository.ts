80e3a2b222dc9bb191e20efee9f189fe0d83a1b4
Alex Colville <56324439+Alexbruvv@users.noreply.github.com>
2025-01-11 00:06:43 +0000

    feat: send stream updates on manual points adjustments

--- packages/server/src/modules/scores/manualPointsAdjustmentsRepository.ts
+++ packages/server/src/modules/scores/manualPointsAdjustmentsRepository.ts
@@ -2,11 +2,25 @@ import { appDb } from "../../db/db";
 import { Repository } from "../../db/repository";
 import type { AppSchema } from "../../db/schema";
 import { manualPointsAdjustments } from "../../db/schema/scores";
+import { stream } from "../../trpc/stream";
 
 class ManualPointsAdjustmentsRepository extends Repository<
     AppSchema,
     AppSchema["manualPointsAdjustments"],
     "manualPointsAdjustments"
-> {}
+> {
+    async afterCreate() {
+        stream.broadcastInvalidateMessage("teams", "fetchAllScores");
+    }
+
+    async afterUpdate() {
+        stream.broadcastInvalidateMessage("teams", "fetchAllScores");
+    }
+
+    async afterDelete() {
+        stream.broadcastInvalidateMessage("teams", "fetchAllScores");
+    }
+}
 
 export const manualPointsAdjustmentsRepository = new ManualPointsAdjustmentsRepository(appDb, manualPointsAdjustments);
+
