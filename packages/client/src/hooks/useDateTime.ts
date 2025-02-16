6eb7d87b76b22ca1bf07c004c7209319a0af1112
Alex Colville <56324439+Alexbruvv@users.noreply.github.com>
2025-02-16 12:59:56 +0000

    fix: attempt to synchronise clocks

--- packages/client/src/hooks/useDateTime.ts
+++ packages/client/src/hooks/useDateTime.ts
@@ -11,11 +11,18 @@ export default function useDateTime(competitionClock?: CompetitionClock) {
     const [now, setNow] = useState(getNow());
 
     useEffect(() => {
-        const interval = setInterval(() => {
-            setNow(getNow());
-        }, 100);
+        let interval: Timer;
+        const timeout = setTimeout(() => {
+            interval = setInterval(() => {
+                setNow(getNow());
+            }, 1050);
+        }, 1000 - now.millisecond);
 
-        return () => clearInterval(interval);
+        return () => {
+            clearTimeout(timeout);
+            clearInterval(interval);
+        };
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [getNow]);
 
     return now;
