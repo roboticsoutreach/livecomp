7687c81347dc102c2573e99ff6ede702a44d145b
Alex Colville <56324439+Alexbruvv@users.noreply.github.com>
2025-02-26 22:04:29 +0000

    feat: add API endpoint for srcomp-live

--- packages/server/src/api/api.ts
+++ packages/server/src/api/api.ts
@@ -1,7 +1,54 @@
 import cors from "@elysiajs/cors";
 import Elysia from "elysia";
+import { competitionsRepository } from "../modules/competitions/competitions.repository";
+import { eq } from "drizzle-orm";
+import { competitions } from "../db/schema/competitions";
+import { CompetitionClock } from "@livecomp/utils";
 
-export const api = new Elysia()
-    .use(cors())
-    .use(new Elysia({ prefix: "api" }).get("now", () => new Date().toISOString()));
+export const api = new Elysia().use(cors()).use(
+    new Elysia({ prefix: "api" })
+        .get("now", () => new Date().toISOString())
+        .get("/:competitionId/live", async ({ params: { competitionId } }) => {
+            const competition = await competitionsRepository.findFirst({
+                where: eq(competitions.id, competitionId),
+                with: {
+                    game: {
+                        with: { startingZones: true },
+                    },
+                    matches: {
+                        with: {
+                            assignments: true,
+                        },
+                    },
+                    matchPeriods: true,
+                    pauses: true,
+                    offsets: true,
+                    venue: true,
+                    teams: true,
+                },
+            });
+
+            if (!competition) return null;
+
+            const competitionClock = new CompetitionClock(competition);
+            const nextMatchId = competitionClock.getNextMatchId();
+            if (!nextMatchId) return { nextMatch: null };
+
+            const nextMatchTimings = competitionClock.getMatchTimings(nextMatchId);
+            if (!nextMatchTimings) return { nextMatch: null };
+
+            const nextMatch = competition.matches.find((match) => match.id === nextMatchId);
+            if (!nextMatch) return { nextMatch: null };
+
+            if (competitionClock.isPaused()) return { nextMatch: null };
+
+            return {
+                nextMatch: {
+                    matchNumber: nextMatch.sequenceNumber,
+                    startsAt: nextMatchTimings.startsAt.toISO(),
+                    now: competitionClock.now().toISO(),
+                },
+            };
+        })
+);
 
